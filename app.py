a1e4dafd8df20fdd442a793df33d3715ffc523be
Joy <kweyuayieko@gmail.com>
2024-08-15 18:30:24 +0300

    feat:fixed merging conflicts

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from functools import wraps
+from dotenv import load_dotenv
+import os
 from flask_restful import reqparse
 import json
 import base64
@@ -15,7 +17,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///airescape.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = "super-secret"
 app.json.compact = False
